c3ed39b437a807133b0d66bbd222f7061048382a
SY101605 <3488124829@qq.com>
2025-09-10 16:24:20 +0800

    successfully submit

--- lab05/src/UnionFind.java
+++ lab05/src/UnionFind.java
@@ -1,29 +1,35 @@
 public class UnionFind {
     // TODO: Instance variables
+    private int[] array;
 
     /* Creates a UnionFind data structure holding N items. Initially, all
        items are in disjoint sets. */
     public UnionFind(int N) {
         // TODO: YOUR CODE HERE
+        array = new int[N];
+        for (int i = 0; i < N; i++){
+            array[i] = -1;
+        }
     }
 
     /* Returns the size of the set V belongs to. */
     public int sizeOf(int v) {
         // TODO: YOUR CODE HERE
-        return -1;
+        return Math.abs(array[find(v)]);
     }
 
     /* Returns the parent of V. If V is the root of a tree, returns the
        negative size of the tree for which V is the root. */
     public int parent(int v) {
         // TODO: YOUR CODE HERE
-        return -1;
+        return array[v];
+
     }
 
     /* Returns true if nodes/vertices V1 and V2 are connected. */
     public boolean connected(int v1, int v2) {
         // TODO: YOUR CODE HERE
-        return false;
+        return find(v1) == find(v2);
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
@@ -31,7 +37,19 @@ public class UnionFind {
        function, throw an IllegalArgumentException. */
     public int find(int v) {
         // TODO: YOUR CODE HERE
-        return -1;
+        if (v < 0 || v > array.length){
+            throw new IllegalArgumentException("Some comment to describe the reason for throwing.");
+        }
+        int root = v;
+        while (parent(root) >= 0){
+            root = parent(root);
+        }
+        while (v != root){
+            int next = array[v];
+            array[v] = root;
+            v = next;
+        }
+        return root;
     }
 
     /* Connects two items V1 and V2 together by connecting their respective
@@ -41,6 +59,12 @@ public class UnionFind {
        already connected should not change the structure. */
     public void union(int v1, int v2) {
         // TODO: YOUR CODE HERE
+        if (sizeOf(v1) < sizeOf(v2)) {
+            array[find(v1)] = find(v2);
+        }else{
+            array[find(v2)] = find(v1);
+        }
+
     }
 
 }
